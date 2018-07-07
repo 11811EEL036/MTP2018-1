80b26a62b2be7163bbbea7b59df12f1a8e76aefa
11811EEL036 <38072643+11811EEL036@users.noreply.github.com>
2018-07-07 10:31:35 -0300

    Update P5.c

--- P5.c
+++ P5.c
@@ -6,34 +6,48 @@ Matricula: 11811EEL036 */
 int main(){
 
 	char str[256];
-	int i, op;
+	int op, i;
 
-	printf("Escolha uma opcao:\n1-Codificar\n2-Descodificar\n");
+    for(i=0; i<256; i++)
+        str[i] = '\0';
+
+	printf("Escolha uma opcao:\n1-Codificar\n2-Decodificar\n");
 	scanf("%d", &op); getchar();
 
-	if(op==1){
-		fgets(str,256,stdin);
+	if(op==1)
+    {
+		for(i=0; i<255; i++)
+		{
+			str[i] = getchar();
+			if(str[i]==10)
+            {
+                str[i]='\0';
+                break;
+            }
+
+		}
 
 		int *pi;
 
 		for(i=0; str[i]!='\0'; i+=4){
 			pi = (int *) &str[i];
 			if(str[i+4]!='\0')
-				printf("%d, ", *pi);
+				printf("%d;", *pi);
 			else
-				printf("%d", *pi);
+				printf("%d\n", *pi);
 		}
 	}else if(op==2){
-		int num[64];
+		int num[64], *pi;
 		char aux;
 
 		for(i=0; i<64; i++){
-			scanf("%d%c", &num[i], &aux);
-			if(aux!=',')
+			scanf("%d", &num[i]);
+			if(num[i]==0)
 				break;
+			pi = (char *) num;
 		}
 
-		printf("%s", num);
+		printf("%s\n", pi);
 	}
 
 	return 0;
